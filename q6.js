04f7ed4af71661bf97853bbd676c636ff8f88b8c
Jassim Ali <jassimali@Jassims-MacBook-Air.local>
2026-09-10 15:34:09 +0530

    corrected the code

--- q6.js
+++ q6.js
@@ -1,3 +1,6 @@
-s="jassim jam jom jim";
-let st=s.replaceAll("j","j".toUpperCase())
-console.log(st)
+s="how are you doing, i want to convert the starting of the each words upper.";
+st=s.split(" ")
+for(let j in st){
+    st[j]=st[j][0].toUpperCase()+ st[j].slice(1);
+    }
+console.log(st.join(" "))
